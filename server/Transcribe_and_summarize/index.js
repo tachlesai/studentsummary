0f649e6de1d9a3fffd2257b06ef361856691b2bf
user <user@users-MacBook-Pro.local>
2025-03-03 20:03:36 +0200

    gfdgf

---
+++ server/Transcribe_and_summarize/index.js
@@ -0,0 +1,11 @@
+// Export all functionality from a single entry point
+export { summarizeText } from './utils.js';
+export { transcribeAudio, processUploadedFile } from './audio.js';
+export { generatePDF } from './pdf.js';
+export { 
+  extractVideoId, 
+  getYouTubeTranscript, 
+  downloadYouTubeAudio,
+  getVideoInfo,
+  processYouTube 
+} from './youtube.js'; 
