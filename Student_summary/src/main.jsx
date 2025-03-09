e863245015d70ca18ed37467e3914dad98113ec0
user <user@users-MacBook-Pro-3.local>
2025-03-10 01:27:15 +0200

    gfdsg

--- Student_summary/src/main.jsx
+++ Student_summary/src/main.jsx
@@ -1,8 +1,18 @@
+import './unregisterServiceWorker';
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/index.css'
-import './unregisterServiceWorker.js'
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.ready.then(registration => {
+    registration.unregister().then(() => {
+      console.log('Service worker successfully unregistered');
+      window.location.reload(); // Force reload after unregistration
+    });
+  });
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
