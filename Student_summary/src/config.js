8e04f52316d03924280127a5145c1e54c4754711
user <user@users-MacBook-Pro-3.local>
2025-03-10 00:19:32 +0200

    gfdsg

--- Student_summary/src/config.js
+++ Student_summary/src/config.js
@@ -1,3 +1,5 @@
-const API_BASE_URL = 'https://207.154.192.212:5001';
+const API_BASE_URL = 'https://tachlesai.com';
+// or better, use your domain:
+// const API_BASE_URL = 'https://tachlesai.com/api';
 
 export default API_BASE_URL; 
