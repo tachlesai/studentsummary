4dbd77a3f661681a89b8f8f3bdb90d7ca93e99c0
user <user@users-MacBook-Pro-3.local>
2025-03-10 01:02:33 +0200

    gfdsg

---
+++ Student_summary/src/unregisterServiceWorker.js
@@ -0,0 +1,9 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for(let registration of registrations) {
+      registration.unregister().then(function() {
+        console.log('ServiceWorker unregistered');
+      });
+    }
+  });
+} 
