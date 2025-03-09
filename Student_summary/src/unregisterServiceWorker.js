435a393421299bc48568b35fd0f293d4bdeb5119
user <user@users-MacBook-Pro-3.local>
2025-03-10 01:43:25 +0200

    gfdsg

--- Student_summary/src/unregisterServiceWorker.js
+++ Student_summary/src/unregisterServiceWorker.js
@@ -1,4 +1,6 @@
+// Immediately unregister any service workers and prevent future registration
 if ('serviceWorker' in navigator) {
+  // First unregister any existing service workers
   navigator.serviceWorker.getRegistrations().then(function(registrations) {
     for(let registration of registrations) {
       registration.unregister().then(function() {
@@ -6,4 +8,19 @@ if ('serviceWorker' in navigator) {
       });
     }
   });
+
+  // Clear any service worker caches
+  caches.keys().then(function(cacheNames) {
+    return Promise.all(
+      cacheNames.map(function(cacheName) {
+        return caches.delete(cacheName);
+      })
+    );
+  });
+
+  // Override the register function to prevent new registrations
+  navigator.serviceWorker.register = function() {
+    console.warn('Service Worker registration prevented');
+    return Promise.reject('Service Worker registration prevented');
+  };
 } 
